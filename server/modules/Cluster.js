b2383c394a142e404197995a68329f5336050d05
siso1119 <chahyunwooa17@gmail.com>
2020-06-19 05:00:52 +0000

    test

--- server/modules/Cluster.js
+++ server/modules/Cluster.js
@@ -1,6 +1,26 @@
 class Cluster{
     constructor(cluster){
-        this.cluster =   cluster || null;
-        this.isMaster = cluster.isMaster;
+        this.cluster =                    cluster || null;
+        this.isMaster = cluster ? cluster.isMaster : null;
+    }
+
+    getCluster(){
+        return this.cluster;
+    }
+
+    getIsMaster(){
+        return this.isMaster;
+    }
+
+    setCluster(cluster){
+        this.cluster =                    cluster || null; 
+        this.isMaster = cluster ? cluster.isMaster : null;
+    }
+    fork(){
+        if(this.getIsMaster()){
+            this.getCluster().fork();
+        } else {
+            throw new Error("this cluster is not master");
+        }
     }
 }
