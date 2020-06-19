93834d5fc1e8d9ab3b415b7c3f13cd7b088b2543
small _Coin <dmsqlctnekf@gmail.com>
2020-06-19 05:54:37 +0000

    rgb

--- client/script/image/Rgb.js
+++ client/script/image/Rgb.js
@@ -1,5 +1,13 @@
 class Rgb {
     constructor(r,g,b){
-        this.r
+        this.r = r || 0;
+        this.g = g || 0;
+        this.b = b || 0
     }
+
+    getAverage(){
+        return this.r
+    }
+
+
 }
